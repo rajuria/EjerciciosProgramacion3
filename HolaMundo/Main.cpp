5eb1f1abbca490ea70eb798922a7811ea4944a26
link5464 <link5464@outlook.es>
2021-10-14 19:55:29 -0600

    Add project files.

---
+++ HolaMundo/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+
+void setup()
+{
+	int Val;
+}
+
+void main()
+{
+	int Val;
+	std::cout<<"Hola Mundo\n";
+	std::cin >> Val;
+	std::cout << "El valor ingresado es: " << Val;
+	_getch();
+}
