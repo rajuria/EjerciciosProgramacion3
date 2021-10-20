241bf9bb322ced6711c9092c3eaccd88fb1618a8
link5464 <link5464@outlook.es>
2021-10-19 20:00:26 -0600

    Agregado Ejemplo de Clases

---
+++ EjemploClase/Main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <conio.h>
+
+class Numero
+{
+private:
+	int valor;
+public:
+	Numero()
+	{
+
+	}
+	Numero(int _valor)
+	{
+
+	}
+	void setValor(int _valor)
+	{
+
+	}
+	int getValor()
+	{
+
+	}
+	bool esPar()
+	{
+
+	}
+	bool esPrimo()
+	{
+
+	}
+	bool esMultiplo(Numero)
+	{
+
+	}
+};
+
+void main()
+{
+
+	_getch();
+}
